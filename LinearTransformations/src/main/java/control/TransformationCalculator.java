a189ae547a4dd26cd8d641b9e2b8cb0132312fa3
priyaVora <prvora89@gmail.com>
2018-04-24 08:30:47 -0600

    Have all Transformation Matrix Formula Calculations!

--- LinearTransformations/src/main/java/control/TransformationCalculator.java
+++ LinearTransformations/src/main/java/control/TransformationCalculator.java
@@ -1,4 +1,4 @@
-package control;
+package control
 
 import java.io.Console;
 import java.util.ArrayList;
@@ -75,7 +75,7 @@ public class TransformationCalculator {
 
 	}
 
-	private Matrix rX(double thetaValues) {
+	public Matrix rX(double thetaValues) {
 		Matrix x = new Matrix();
 		double[][] currentX = new double[3][3];
 		currentX[0][0] = 1;
@@ -93,7 +93,7 @@ public class TransformationCalculator {
 		return x;
 	}
 
-	private Matrix rY(double thetaValues) {
+	public Matrix rY(double thetaValues) {
 		Matrix y = new Matrix();
 		double[][] currentY = new double[3][3];
 		currentY[0][0] = cosineTheta(thetaValues);
@@ -111,7 +111,7 @@ public class TransformationCalculator {
 		return y;
 	}
 
-	private Matrix rZ(double thetaValues) {
+	public Matrix rZ(double thetaValues) {
 		Matrix z = new Matrix();
 		double[][] currentZ = new double[3][3];
 		currentZ[0][0] = cosineTheta(thetaValues);
@@ -147,24 +147,298 @@ public class TransformationCalculator {
 		return resultMatrix;
 	}
 
-	public void scale() {
+	public Matrix scale_2D(double kx, double ky) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = kx;
+		resultValue[0][1] = 0;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = ky;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix scale_2D_Vectors(double k, double nx, double ny) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = (1 + ((k - 1) * (nx * nx)));
+		resultValue[0][1] = (((k - 1) * (nx * ny)));
+
+		resultValue[1][0] = (((k - 1) * (nx * ny)));
+		resultValue[1][1] = (1 + ((k - 1) * (ny * ny)));
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix scale_3D(double kx, double ky, double kz) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[3][3];
+		resultValue[0][0] = kx;
+		resultValue[0][1] = 0;
+		resultValue[0][2] = 0;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = ky;
+		resultValue[1][2] = 0;
+
+		resultValue[2][0] = 0;
+		resultValue[2][1] = 0;
+		resultValue[2][2] = kz;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix scale_3D_Vector(double k, double nx, double ny, double nz) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[3][3];
+		resultValue[0][0] = (1 + ((k - 1) * (nx * nx)));
+		resultValue[0][1] = (((k - 1) * (nx * ny)));
+		resultValue[0][2] = ((k - 1) * (nx * nz));
+
+		resultValue[1][0] = ((k - 1) * (nx * ny));
+		resultValue[1][1] = (1 + ((k - 1) * (ny * ny)));
+		resultValue[1][2] = ((k - 1) * (ny * nz));
+
+		resultValue[2][0] = ((k - 1) * (nx * nz));
+		resultValue[2][1] = ((k - 1) * (ny * nz));
+
+		resultValue[2][2] = (1 + ((k - 1) * (nz * nz)));
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix orthographicProjection_2D_Horizontal() {
+		Matrix resultMatrix = new Matrix();
+		double[][] p = new double[2][2];
+		p[0][0] = 1;
+		p[0][1] = 0;
+		p[1][0] = 0;
+		p[1][1] = 0;
+		resultMatrix.setCurrentMatrix(p);
+		return resultMatrix;
+	}
+
+	public Matrix orthographicProjection_2D_Vertical() {
+		Matrix resultMatrix = new Matrix();
+		double[][] p = new double[2][2];
+		p[0][0] = 0;
+		p[0][1] = 0;
+		p[1][0] = 0;
+		p[1][1] = 1;
+		resultMatrix.setCurrentMatrix(p);
+		return resultMatrix;
+	}
+
+	public Matrix orthographicProjection_3D(double nx, double ny, double nz) {
+		Matrix resultMatrix = new Matrix();
+		double[][] currentValues = new double[3][3];
+		currentValues[0][0] = (1 - (nx * nx));
+		currentValues[0][1] = (-nx * ny);
+		currentValues[0][2] = (-nx * nz);
+
+		currentValues[1][0] = (-nx * ny);
+		currentValues[1][1] = (1 - (ny * ny));
+		currentValues[1][2] = (-ny * nz);
+
+		currentValues[2][0] = (-nx * nz);
+		currentValues[2][1] = (-ny * nz);
+		currentValues[2][2] = (1 - (nz * nz));
+
+		resultMatrix.setCurrentMatrix(currentValues);
+		return resultMatrix;
+	}
+
+	public Matrix reflection_Horizontal_2D() {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = 0;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = -1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix reflection_Vertical_2D() {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = -1;
+		resultValue[0][1] = 0;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix reflection_Vector(double nx, double ny) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = (1 - (2 * (nx * nx)));
+		resultValue[0][1] = (-2 * nx * ny);
+
+		resultValue[1][0] = (-2 * nx * ny);
+		resultValue[1][1] = (1 - (2 * (ny * ny)));
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix reflection_3D(double nx, double ny, double nz) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[3][3];
+		resultValue[0][0] = (1 - (2 * (nx * nx)));
+		resultValue[0][1] = (-2 * nx * ny);
+		resultValue[0][2] = (-2 * nx * nz);
+
+		resultValue[1][0] = (-2 * nx * ny);
+		resultValue[1][1] = (1 - (2 * (ny * ny)));
+		resultValue[1][2] = (-2 * ny * nz);
+
+		resultValue[2][0] = (-2 * nx * nz);
+		resultValue[2][1] = (-2 * ny * nz);
+		resultValue[2][2] = (1 - (2 * (nz * nz)));
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
 
 	}
 
-	public void orthographicProjection() {
+	public Matrix shear_Horizontal(double k) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = k;
 
+		resultValue[1][0] = 0;
+		resultValue[1][1] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
 	}
 
-	public void reflection() {
+	public Matrix shear_Vertical(double k) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = 0;
 
+		resultValue[1][0] = k;
+		resultValue[1][1] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
 	}
 
-	public void shear() {
+	public Matrix shear_3rdRow(double k1, double k2) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = 0;
+		resultValue[0][2] = 0;
 
+		resultValue[1][0] = k1;
+		resultValue[1][1] = 1;
+		resultValue[1][2] = 0;
+
+		resultValue[2][0] = k2;
+		resultValue[2][1] = 0;
+		resultValue[2][2] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
 	}
 
-	public void translation() {
+	public Matrix shear_2ndRow(double k1, double k2) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = k2;
+		resultValue[0][2] = 0;
 
+		resultValue[1][0] = 0;
+		resultValue[1][1] = 1;
+		resultValue[1][2] = 0;
+
+		resultValue[2][0] = 0;
+		resultValue[2][1] = k2;
+		resultValue[2][2] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix shear_1stRow(double k1, double k2) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[2][2];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = 0;
+		resultValue[0][2] = k1;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = 1;
+		resultValue[1][2] = k2;
+
+		resultValue[2][0] = 0;
+		resultValue[2][1] = 0;
+		resultValue[2][2] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix translation_2D(double xChange, double yChange) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[3][3];
+		resultValue[0][0] = 1;
+		resultValue[0][1] = 0;
+		resultValue[0][2] = xChange;
+
+		resultValue[1][0] = 0;
+		resultValue[1][1] = 1;
+		resultValue[1][2] = yChange;
+
+		resultValue[2][0] = 0;
+		resultValue[2][1] = 0;
+		resultValue[2][2] = 1;
+
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
+	}
+
+	public Matrix translation_3D(double xChange, double yChange, double zChange) {
+		Matrix resultMatrix = new Matrix();
+		double[][] resultValue = new double[4][4];
+
+		resultValue[0][1] = 1;
+		resultValue[0][2] = 0;
+		resultValue[0][3] = 0;
+		resultValue[0][4] = xChange;
+
+		resultValue[1][1] = 0;
+		resultValue[1][2] = 1;
+		resultValue[1][3] = 0;
+		resultValue[1][4] = yChange;
+
+		resultValue[2][0] = 0;
+		resultValue[2][1] = 0;
+		resultValue[2][2] = 1;
+		resultValue[2][3] = zChange;
+
+		resultValue[3][0] = 0;
+		resultValue[3][1] = 0;
+		resultValue[3][2] = 0;
+		resultValue[3][3] = 1;
+		resultMatrix.setCurrentMatrix(resultValue);
+		return resultMatrix;
 	}
 
 	public DimensionType getDimension() {
